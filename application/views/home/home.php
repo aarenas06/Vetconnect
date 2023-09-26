5cb094544f5dbf56894d08cdba11c60a457219eb
aarenas06 <diegoaarenas06@gmail.com>
2023-09-25 21:54:13 -0500

    este es el commit del main

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,4 +1,14 @@
 <div class="contenido" style="padding-top: calc(70px + 15px);">
-
     <h1>Holi este es el home de Usuarios</h1>
+
+    <div class="card">
+        <div class="card-body" style="background-color: red;">
+            <p style="color: white;">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus rerum veritatis
+                optio eos ullam exercitationem enim ut? Nemo expedita dolores earum fuga, hic accusamus,
+                eaque consectetur et beatae corrupti error.
+            </p>
+
+        </div>
+    </div>
 </div>
