b4eb19338f51867191365520014acf26d88da5e1
aarenas06 <diegoaarenas06@gmail.com>
2023-09-25 21:31:20 -0500

    Se agregaron las cartas correspondientes

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,4 +1,69 @@
 <div class="contenido" style="padding-top: calc(70px + 15px);">
-
-    <h1>Holi este es el home de Usuarios</h1>
+    <!--  InfoGraficas -->
+    <div class="owl-carousel counter-carousel owl-theme">
+        <div class="item">
+            <div class="card border-0 zoom-in bg-light-primary shadow-none">
+                <div class="card-body">
+                    <div class="text-center">
+                        <center>
+                            <img src="<?= base_url() ?>assets/img/home/icons/doc.png" width="70" height="50" class="mb-3" alt="" />
+                        </center>
+                        <p class="fw-semibold fs-3 text-primary mb-1"> Empleados </p>
+                        <h5 class="fw-semibold text-primary mb-0">10</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="card border-0 zoom-in bg-light-warning shadow-none">
+                <div class="card-body">
+                    <div class="text-center">
+                        <center>
+                            <img src="<?= base_url() ?>assets/img/home/icons/user.png" width="70" height="50" class="mb-3" alt="" />
+                        </center>
+                        <p class="fw-semibold fs-3 text-warning mb-1">Clientes</p>
+                        <h5 class="fw-semibold text-warning mb-0">2,650</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="card border-0 zoom-in bg-light-danger shadow-none">
+                <div class="card-body">
+                    <div class="text-center">
+                        <center>
+                            <img src="<?= base_url() ?>assets/img/home/icons/dog.png" width="70" height="50" class="mb-3" alt="" />
+                        </center>
+                        <p class="fw-semibold fs-3 text-danger mb-1">Pacientes</p>
+                        <h5 class="fw-semibold text-danger mb-0">4,685</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="card border-0 zoom-in bg-light-success shadow-none">
+                <div class="card-body">
+                    <div class="text-center">
+                        <center>
+                            <img src="<?= base_url() ?>assets/img/home/icons/ganancias.png" width="70" height="50" class="mb-3" alt="" />
+                        </center>
+                        <p class="fw-semibold fs-3 text-success mb-1">Ganancias</p>
+                        <h5 class="fw-semibold text-success mb-0">$96k</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="card border-0 zoom-in bg-light-info shadow-none">
+                <div class="card-body">
+                    <div class="text-center">
+                        <center> <img src="<?= base_url() ?>assets/img/home/icons/ticket.png" width="70" height="50" class="mb-3" alt="" />
+                        </center>
+                        <p class="fw-semibold fs-3 text-info mb-1">Tickets</p>
+                        <h5 class="fw-semibold text-info mb-0">10</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
